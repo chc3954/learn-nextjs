6c43fb53d1513f2334c386d6a85bce0b50115481
Hyunchul <chc3954@gmail.com>
2024-08-10 21:56:56 -0400

    Updated movie detail page and its styles

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,11 +1,23 @@
 import { getMovie } from "@/app/utility";
+import styles from "@/app/styles/movie-info.module.css";
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <p>{movie.overview}</p>
+    <div className={styles.container}>
+      <img
+        className={styles.poster}
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️{movie.vote_average.toFixed(2)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank" rel="noreferrer">
+          Homepage →
+        </a>
+      </div>
     </div>
   );
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,6 +1,18 @@
 import { getVideos } from "@/app/utility";
+import styles from "@/app/styles/movie-video.module.css";
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <div>{JSON.stringify(videos)}</div>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video: any) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}></iframe>
+      ))}
+    </div>
+  );
 }
